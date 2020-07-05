08e5ed8cf5e2870d8824ca7d686be0a65ed22204
Felipe Miranda <felip.simi@gmail.com>
2020-07-05 13:48:21 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/techtrilha/trilhainicial/globalerrors/GlobalErrorsApplication.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.globalerrors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalErrorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalErrorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtrilha/trilhainicial/globalerrors/GlobalErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtrilha.trilhainicial.globalerrors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalErrorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
